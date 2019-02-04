dccfd8d544bc9c60d97d5a1e4081558d84f63fe2
Vanilna <lesyalp@gmail.com>
2019-02-04 12:15:06 +0100

    Tring to make form validation - all empty elements should turn red and validation should be prevented. All works well exept terning to red =D.

--- script.js
+++ script.js
@@ -16,12 +16,29 @@
         return JSON.stringify(person);
     }
 
+    var counter;
+
+    function formValidation () {
+        counter = 0;
+        var elementsRequired = document.querySelectorAll(".required");
+        for (i = 0; i < elementsRequired.length; i++) {
+            if (elementsRequired[i].value) {
+                console.log(1);
+                elementsRequired[i].style.backgroundColor = "red";
+                counter++;
+            } 
+        }
+    }
+
     document.addEventListener("DOMContentLoaded", function () {
         var checkinForm = document.getElementById("checkin-form");
         checkinForm.addEventListener("submit", function (evnt) {
             evnt.preventDefault();
+            formValidation();
+            if (counter === 0) {
             var personJSON = createJSON(this);
             console.log(personJSON);
+            }
         }, false);
     });
 })();
