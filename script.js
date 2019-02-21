39ed3c6b230ad7b4655ee759c11e3095f4960de8
Vanilna <lesyalp@gmail.com>
2019-02-21 17:40:44 +0100

    Setteled a js code for invoice checkbox - now when clicked it anabeless and disables the invoice form.

--- script.js
+++ script.js
@@ -16,9 +16,9 @@
         return JSON.stringify(person);
     }
 
-   var counter;
+    var counter;
 
-    function formValidation () {
+    function formValidation() {
         counter = 0;
         var elementsRequired = document.querySelectorAll(".required");
         var element;
@@ -27,25 +27,47 @@
             if (element.value == false || "on") {
                 element.style.boxShadow = "inset 0em 0em 0.3em red";
                 counter++;
-            } 
+            }
         }
     }
 
     document.addEventListener("DOMContentLoaded", function () {
         var checkinForm = document.getElementById("checkin-form");
 
-        checkinForm.addEventListener("focus", function(e){
-            console.log(1);
+        checkinForm.addEventListener("focus", function (e) {
             e.target.style.boxShadow = "";
+        }, true);
+
+        checkinForm.addEventListener("blur", function (ev) {
+            if (ev.target.invalid) {
+                console.log(1);
+            }
         }, true);
 
         checkinForm.addEventListener("submit", function (evnt) {
             evnt.preventDefault();
             formValidation();
             if (counter === 0) {
-            var personJSON = createJSON(this);
-            console.log(personJSON);
+                var personJSON = createJSON(this);
+                console.log(personJSON);
             }
         }, false);
     });
 })();
+
+document.querySelector("#invoceAggree").addEventListener("change", function () {
+    var invoice = document.querySelector(".invoice");
+    
+
+    if (this.checked) {
+        var disabled = invoice.querySelectorAll("input[disabled]");
+        for (i = 0; i < disabled.length; i++) {
+            disabled[i].removeAttribute("disabled");
+        }
+    } else {
+        var toDisable = invoice.querySelectorAll("input");
+        for (i = 1; i < toDisable.length; i++) {
+            toDisable[i].setAttribute("disabled", "");
+        }
+    }
+});
